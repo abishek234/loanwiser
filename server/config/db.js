b11add886409bc19a70a369859e9553098108562
abishek <avineshmtp@gmail.com>
2025-03-18 01:00:28 +0530

    update db

--- server/config/db.js
+++ server/config/db.js
@@ -8,9 +8,11 @@ const pool = mysql.createPool({
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'loanwiser',
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
+  queueLimit: 0,
+  connectTimeout: 30000 
 });
 
 async function connectDB() {
